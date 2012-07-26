cc364786937f74ccb945c90d8d9610c15e52f1d6
syshen <sysheen@gmail.com>
2012-07-26 17:15:24 +0800

    Fix missing delegate synthesize

--- IRAQAlbumListViewController.m
+++ IRAQAlbumListViewController.m
@@ -24,6 +24,7 @@
 @synthesize assetsLibrary = _assetsLibrary;
 @synthesize assetsGroupType = _assetsGroupType;
 @synthesize assetGroups = _assetGroups;
+@synthesize delegate = _delegate;
 
 - (id) initWithAssetsLibrary:(ALAssetsLibrary *)library groupType:(ALAssetsGroupType)groupType {
 
